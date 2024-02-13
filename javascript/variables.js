37635e1433b857bc8ca4eb5987a11bbeeb6cf5ec
MatiasMiguelGomez <miguel_gomez30@outlook.com>
2024-02-13 14:55:33 -0300

    probando git 1

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,6 +1,7 @@
-export{url}
+export{url, galeriaCartas}
 // variables generales
-let url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+let url = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+let galeriaCartas = document.getElementById("galeriaCartas");
 // variables header
 //variables filtros
 //variables galeria
